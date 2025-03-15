29aeccf83d6f06ce1443d58fc841145010d7fae2
PSIR <79282746+psir466@users.noreply.github.com>
2025-03-15 18:10:58 +0100

    read excel

--- myfin-app-batch/src/main/java/org/psi/myfinappbatch/configuration/TaskletBatchConfiguration.java
+++ myfin-app-batch/src/main/java/org/psi/myfinappbatch/configuration/TaskletBatchConfiguration.java
@@ -11,7 +11,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.springframework.batch.core.Job;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 @Configuration
 //@EnableBatchProcessing (nee pas mettre dans spring boot 3 car sinon le job ne démarre pas au démarrage de l'application )
@@ -27,7 +39,28 @@ public class TaskletBatchConfiguration {
     public Tasklet myTasklet() {
         return (contribution, chunkContext) -> {
             System.out.println("Executing my tasklet...");
-            // Your task logic here
+            
+
+            FileInputStream file = new FileInputStream("C:\\Users\\psir1\\Documents\\Perso\\budget\\budget2.xlsx");
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            for (int rowIndex = 0; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                XSSFRow row = sheet.getRow(rowIndex);
+                if (row != null) {
+                    for (int cellIndex = 0; cellIndex < row.getLastCellNum(); cellIndex++) {
+                        XSSFCell cell = row.getCell(cellIndex);
+                        if (cell != null) {
+                            System.out.print(cell.toString() + "\t");
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+
+            workbook.close();
+            file.close();
+
             System.out.println("Tasklet execution complete.");
             return RepeatStatus.FINISHED;
         };
